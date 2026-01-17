0ec2d03842d224258ec8a9c3686d235297f67d1e
Md. Alinur Hossain <alinur1160@gmail.com>
2026-01-17 14:27:20 +0600

    Headers added in missing files

--- src/renderer/tools/redactPdf/redactPdf.js
+++ src/renderer/tools/redactPdf/redactPdf.js
@@ -1,3 +1,21 @@
+/**
+ * LocalPDF Studio - Offline PDF Toolkit
+ * ======================================
+ * 
+ * @author      Md. Alinur Hossain <alinur1160@gmail.com>
+ * @license     AGPL 3.0 (GNU Affero General Public License version 3)
+ * @website     https://alinur1.github.io/LocalPDF_Studio_Website/
+ * @repository  https://github.com/Alinur1/LocalPDF_Studio
+ * 
+ * Copyright (c) 2025 Md. Alinur Hossain. All rights reserved.
+ * 
+ * Architecture:
+ * - Frontend: Electron + HTML/CSS/JS
+ * - Backend: ASP.NET Core Web API, Python
+ * - PDF Engine: PdfSharp + Mozilla PDF.js
+**/
+
+
 // src/renderer/tools/redactPdf/redactPdf.js
 
 import { API } from '../../api/api.js';
