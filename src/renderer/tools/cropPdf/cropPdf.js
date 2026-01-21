112a32dae5e852d817be38efa890bb541e30b5d5
Md. Alinur Hossain <alinur1160@gmail.com>
2026-01-21 19:59:00 +0600

    Light mode support added for Crop PDF

--- src/renderer/tools/cropPdf/cropPdf.js
+++ src/renderer/tools/cropPdf/cropPdf.js
@@ -23,11 +23,13 @@ import customAlert from "../../utils/customAlert.js";
 import loadingUI from "../../utils/loading.js";
 import { initializeGlobalDragDrop } from '../../utils/globalDragDrop.js';
 import i18n from "../../utils/i18n.js";
+import { ThemeManager } from "../../utils/themeManager.js";
 pdfjsLib.GlobalWorkerOptions.workerSrc = "../../../pdf/build/pdf.worker.mjs";
 
 document.addEventListener("DOMContentLoaded", async () => {
   await API.init();
   await i18n.init();
+  ThemeManager.init();
 
   const selectPdfBtn = document.getElementById("select-pdf-btn");
   const removePdfBtn = document.getElementById("remove-pdf-btn");
