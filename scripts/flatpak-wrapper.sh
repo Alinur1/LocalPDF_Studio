46153cab7145908adc5a5e55967d734ed6e36f4e
Md. Alinur Hossain <alinur1160@gmail.com>
2026-02-04 21:17:23 +0600

    Revert "Flatpak builder + guide docs added"
    This reverts commit 12db062d05a4da3ed405785de1a638a39a76c27f.

--- scripts/flatpak-wrapper.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-export PATH="/app/main/resources/assets/backend_linux/compiled-ghostscript/bin:$PATH"
-
-# "$@" ensures that the file path (e.g. /home/user/file.pdf) is passed to the app
-exec /app/main/localpdf-studio --no-sandbox "$@"
