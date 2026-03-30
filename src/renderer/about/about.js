d025a4241bf73245fb9058f398b4a840562c1d6f
Md. Alinur Hossain <alinur1160@gmail.com>
2026-03-30 20:49:01 +0600

    Updated about page

--- src/renderer/about/about.js
+++ src/renderer/about/about.js
@@ -55,7 +55,6 @@ function setupButtons() {
     const instagramBtn = document.getElementById('instagram-btn');
     const redditBtn = document.getElementById('reddit-btn');
     const currentlyWorkingOnBtn = document.getElementById('currently-working-on');
-    const starRepoBtn = document.getElementById('star-repo');
     const reportIssueBtn = document.getElementById('report-issue');
     const releaseNoteBtn = document.getElementById('release-note');
 
@@ -64,9 +63,6 @@ function setupButtons() {
     }
     if (currentlyWorkingOnBtn) {
         currentlyWorkingOnBtn.addEventListener('click', () => openExternalLink('https://docs.google.com/document/d/1wcbxeCYDs7yDEdKZFABC8Jow5mxG0XUqfbuB_fO3-hI/edit?usp=sharing'));
-    }
-    if (starRepoBtn) {
-        starRepoBtn.addEventListener('click', () => openExternalLink('https://github.com/Alinur1/LocalPDF_Studio'));
     }
     if (reportIssueBtn) {
         reportIssueBtn.addEventListener('click', () => openExternalLink('https://github.com/Alinur1/LocalPDF_Studio/issues'));
