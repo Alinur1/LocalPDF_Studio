cc889911e54bc407e64fc887fbac34ba40ed634c
Md. Alinur Hossain <alinur1160@gmail.com>
2026-04-01 18:41:30 +0600

    Revert "Emoji added in png format"
    This reverts commit 76adcb9fc280b9a24213e684d7849612798de81a.

--- src/renderer/utils/searchBar.js
+++ src/renderer/utils/searchBar.js
@@ -19,7 +19,6 @@
 // src/renderer/utils/searchBar.js
 
 import i18n from "./i18n.js";
-import { loadEmojiImagesIn } from "./emojiLoader.js";
 
 export class SearchBar {
     constructor(searchIndexManager, tabManager) {
@@ -41,13 +40,10 @@ export class SearchBar {
         this.container.innerHTML = `
             <div class="search-input-wrapper">
                 <input type="text" id="pdf-search" placeholder="" class="search-input">
-                <span class="search-icon">
-                    <span data-emoji="mag" width="15" height="15"></span>
-                </span>
+                <span class="search-icon">🔍</span>
             </div>
             <div class="search-results hidden"></div>
         `;
-        loadEmojiImagesIn(this.container);
 
         this.input = this.container.querySelector('#pdf-search');
         this.results = this.container.querySelector('.search-results');
