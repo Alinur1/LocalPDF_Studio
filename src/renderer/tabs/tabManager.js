5fe03b09af80fea13578fa282b2441c72fe2bf97
Md. Alinur Hossain <alinur1160@gmail.com>
2026-04-02 00:14:53 +0600

    Emojis replaced with SVGs
    - PDF Reader
    - About page
    - Donate page

--- src/renderer/tabs/tabManager.js
+++ src/renderer/tabs/tabManager.js
@@ -183,7 +183,7 @@ export default class TabManager {
 
         const closeBtn = document.createElement('span');
         closeBtn.classList.add('tab-close');
-        closeBtn.textContent = '×';
+        closeBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 19" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x-icon lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>';
         closeBtn.addEventListener('click', (e) => {
             e.stopPropagation();
             this.closeTab(id);
--- src/renderer/utils/searchBar.js
+++ src/renderer/utils/searchBar.js
@@ -40,7 +40,9 @@ export class SearchBar {
         this.container.innerHTML = `
             <div class="search-input-wrapper">
                 <input type="text" id="pdf-search" placeholder="" class="search-input">
-                <span class="search-icon">🔍</span>
+                <span class="search-icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 20" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search-icon lucide-search"><path d="m21 21-4.34-4.34"/><circle cx="11" cy="11" r="8"/></svg>
+                </span>
             </div>
             <div class="search-results hidden"></div>
         `;
